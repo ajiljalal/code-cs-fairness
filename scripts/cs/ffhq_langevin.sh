c0a7ed265ff0ad18a14e5c57a290c57e25b34de4
Ajil Jalal <ajiljalal@gmail.com>
2021-06-18 09:51:02 -0500

    initial commit

---
+++ scripts/cs/ffhq_langevin.sh
@@ -0,0 +1,2 @@
+python src/compressed_sensing.py --checkpoint-path checkpoints/ncsnv2_ffhq/checkpoint_80000.pth --net ncsnv2 --dataset ffhq-69000 --num-input-images 1 --batch-size 1 --ncsnv2-configs-file ./ncsnv2/configs/ffhq.yml --measurement-type circulant --noise-std 0 --num-measurements 20000 --model-type langevin --print-stats --checkpoint-iter 1 --cuda --mloss-weight 1.0 --learning-rate 9e-6 --sigma-init 348 --sigma-final 0.01 --L 2311 --T 3 --not-lazy
+
